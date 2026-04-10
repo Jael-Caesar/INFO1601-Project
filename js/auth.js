95b8e069fc634a583484fe463bd982425cbb9147
Jael-Caesar <jaelsacaesar@gmail.com>
2026-04-10 14:08:11 -0400

    Update Sign Up and Log In

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -47,10 +48,46 @@ export async function handleSignUp(email, password) {
     }
 }
 
-const loginForm = document.querySelector('form');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Stops page from reloading
     const email = loginForm.querySelectorAll('input')[1].value;
     const password = loginForm.querySelectorAll('input')[2].value;
     handleLogin(email, password);
 });
+
+const loginForm = document.querySelector('#login-form');
+const signupForm = document.querySelector('#signup-form');
+
+// LOGIN LOGIC
+if (loginForm) {
+    loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const email = loginForm.querySelector('.email-field').value;
+        const password = loginForm.querySelector('.password-field').value;
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            sessionStorage.setItem('isLoggedIn', 'true');
+            window.location.href = 'shop.html'; // Successful login redirect
+        } catch (error) {
+            alert("Error: " + error.message);
+        }
+    });
+}
+
+// SIGN-UP LOGIC
+if (signupForm) {
+    signupForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const email = signupForm.querySelector('.email-field').value;
+        const password = signupForm.querySelector('.password-field').value;
+
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            alert("Account created! Now please log in.");
+            window.location.href = 'log-in.html';
+        } catch (error) {
+            alert("Error: " + error.message);
+        }
+    });
+}

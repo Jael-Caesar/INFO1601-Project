0be9fdc0e4af240304e5a60ca6d3e594c7df45a6
Jael-Caesar <jaelsacaesar@gmail.com>
2026-04-10 20:21:46 -0400

    Update to show config

---
+++ js/config.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+    PERENUAL_KEY: 'sk-eIHc69cfe9baf373016093',
+    
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyAcgH1BvNrp6dP57MIRjmyGxF7piZ7gQ5Y",
+        authDomain: "aqua-fern.firebaseapp.com",
+        projectId: "aqua-fern",
+        storageBucket: "aqua-fern.appspot.com",
+        messagingSenderId: "123456789",
+        appId: "1:123456789:web:abcdef"
+    }
+};

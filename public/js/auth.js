6a4033baddfa281f7cefca62a37390b7aa3d9a84
Jael-Caesar <jaelsacaesar@gmail.com>
2026-04-12 11:20:39 -0400

    updates to signup

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,12 +1,12 @@
 import { CONFIG } from './config.js';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.x.x/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import { 
     getAuth, 
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword,
     onAuthStateChanged,
     signOut
-} from "https://www.gstatic.com/firebasejs/9.x.x/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 
 const app = initializeApp(CONFIG.FIREBASE_CONFIG);
 const auth = getAuth(app);
